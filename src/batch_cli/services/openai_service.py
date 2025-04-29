075e38361312445bce6096bdb493190a15b0e7c7
mmysior <mysior.marek@gmail.com>
2025-04-29 18:23:59 +0200

    Add OpenAIService class for handling OpenAI completions with optional instructor integration

---
+++ src/batch_cli/services/openai_service.py
@@ -0,0 +1,33 @@
+from typing import List
+
+import instructor
+from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
+
+from batch_cli.models.schemas import Body, MessageModel
+
+load_dotenv()
+
+type LLMClient = OpenAI | instructor.Instructor
+
+
+class OpenAIService:
+    def __init__(self, patched: bool = False):
+        self.patched: bool = patched
+        self.client: LLMClient = self._get_client()
+
+    def _get_client(self) -> LLMClient:
+        client: OpenAI = OpenAI(
+            base_url="http://localhost:11434/v1/",
+            api_key="ollama",
+        )
+        if self.patched:
+            return instructor.from_openai(client)
+        return client
+
+    def create_completion(
+        self, messages: List[MessageModel], **kwargs
+    ) -> ChatCompletion:
+        completion_params = Body(messages=messages, **kwargs)
+        return self.client.chat.completions.create(**completion_params.model_dump())
